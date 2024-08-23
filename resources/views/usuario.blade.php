172e86ed35a7e21812f268780f07e844305b2dac
jaglorena <loremedi0103@gmail.com>
2024-08-23 13:03:51 -0600

    cambios de login

--- resources/views/usuario.blade.php
+++ resources/views/usuario.blade.php
@@ -11,29 +11,8 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-            <!--<a class="nav-link" href="#"><img src="https://i.ibb.co/pZsqW4J/Logo-Melodies-Li.png" alt="User" style="width: 30px; border-radius: 50%;"></a>-->
-            <a class="nav-link" href="#"><img src="https://i.ibb.co/JvTD1Xx/Logo-Melodies-Li-1.png" alt="User" style="width: 30px; border-radius: 50%;"></a>
-            <a class="navbar-brand color-word" href="#">Melodies Li</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link color-word" href="#">Usuario</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="https://cdn.pixabay.com/photo/2022/11/22/22/38/woman-7610770_1280.png" alt="User" style="width: 30px; border-radius: 50%;"></a>
-                    </li>
-                </ul>
-                <form action="/logout" method="POST" class="logout-form">
-                    <button type="submit" class="logout-button">Cerrar sesión</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+@include('navbar.navbar')
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <div class="container music-section mt-4">
         <div class="row">
