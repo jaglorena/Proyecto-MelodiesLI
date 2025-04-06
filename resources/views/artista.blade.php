72537a9d8a9859d8a5e4c8c8bca91752c62b914f
jaglorena <115682472+jaglorena@users.noreply.github.com>
2025-04-06 17:14:08 -0600

    Cambios a navbar (#15)

--- resources/views/artista.blade.php
+++ resources/views/artista.blade.php
@@ -1,30 +1,22 @@
-<!DOCTYPE html>
-<html lang="es">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Artista</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/artista.css') }}">
-</head>
+@section('title', 'Panel Artista - Melodies Li')
 
-<body>
-    @include('navbar.navbar')
-    <div class="container">
-        <h1>Opciones de Artista</h1>
-        <h2>Regalías Generadas</h2>
-        <div class="section">
-            <div class="row">
-                <div class="col">
-                    <h3 class="text-center" style="color: white;">Funcionalidad bajo desarrollo</h3>
-                </div>
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Opciones de Artista</h1>
+
+    <h2>Regalías Generadas</h2>
+    <div class="section">
+        <div class="row">
+            <div class="col">
+                <h3 class="text-center text-white">Funcionalidad bajo desarrollo</h3>
             </div>
         </div>
     </div>
+</div>
+@endsection
 
+@section('scripts')
     <script src="{{ asset('js/app.js') }}"></script>
-</body>
-
-</html>
+@endsection
--- resources/views/genero.blade.php
+++ resources/views/genero.blade.php
@@ -1,52 +1,46 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>G&eacute;nero</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+@extends('layouts.app')
 
-</head>
-<body>
-    @include('navbar.navbar')
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>G&eacute;nero</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                @if (isset($genero))
-                    <form>
-                        @csrf
-                        <div class="mb-3">
-                            <label for="nombre" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="nombreHelp" value="{{$genero->nombre}}" readonly>
-                            <div id="nombreHelp" class="form-text">Nombre del g&eacute;nero</div>
-                        </div>
-                    </form>
-                @else
-                    <form method="POST" action="{{ route('guardarGenero') }}">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="nombre" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="nombreHelp">
-                            <div id="nombreHelp" class="form-text">Nombre del g&eacute;nero</div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <button type="submit" class="btn btn-primary">Guardar</button>
-                            </div>
-                        </div>
-                    </form>
-                @endif
-            </div>
+@section('title', 'Género')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Género</h1>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</body>
-</html>
+    <div class="row">
+        <div class="col">
+            @if (isset($genero))
+                <form>
+                    @csrf
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="nombreHelp" value="{{ $genero->nombre }}" readonly>
+                        <div id="nombreHelp" class="form-text">Nombre del género</div>
+                    </div>
+                </form>
+            @else
+                <form method="POST" action="{{ route('guardarGenero') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="nombreHelp">
+                        <div id="nombreHelp" class="form-text">Nombre del género</div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </div>
+                    </div>
+                </form>
+            @endif
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+@endsection
