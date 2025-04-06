d2b594740f5c1a3d5819ffe3b5d2acb233002e6b
Ivonne Duran <111211850+ivonne-duran@users.noreply.github.com>
2025-04-06 17:03:59 -0600

    Utenticacion y autorizacion (#14)

--- routes/web.php
+++ routes/web.php
@@ -7,41 +7,46 @@ use App\Http\Controllers\CancionController;
 use App\Http\Controllers\GeneroController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegaliasController;
-use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Middleware\PermisosAdmin;
 use Illuminate\Support\Facades\Route;
-
-
 
 // Página de inicio de sesión
 Route::get('/', [LoginController::class, 'showLogin'])->name('login');
 
-// Página de 
+// Página de registro
 Route::get('/registro', [UsuarioController::class, 'registro'])->name('registro');
 Route::get('/registro', [RegistroController::class, 'showRegistro']);
 Route::post('/registro', [RegistroController::class, 'registro'])->name('registrarUsuario');
 
 // Enviar formulario de inicio de sesión
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
-
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/usuario', [UsuarioController::class, 'showUsuario']);
-Route::get('/admin', [AdminController::class, 'showAdmin']);
-Route::get('/artista', [ArtistaController::class, 'showArtista']);
-Route::get('/artista/{id}', [ArtistaController::class, 'show']);
-Route::post('/adminartista', [ArtistaController::class, 'store'])->name('guardarArtista');
-Route::get('/adminartista', [ArtistaController::class, 'index']);
-Route::get('/cancion/{id}', [CancionController::class, 'show']);
-Route::get('/cancion', [CancionController::class, 'index']);
-Route::post('/cancion', [CancionController::class, 'store'])->name('guardarCancion');
-Route::get('/album', [AlbumController::class, 'index']);
-Route::get('/album/{id}', [AlbumController::class, 'show']);
-Route::post('/album', [AlbumController::class, 'store'])->name('guardarAlbum');
-Route::get('/genero', [GeneroController::class, 'index']);
-Route::get('/genero/{id}', [GeneroController::class, 'show']);
-Route::post('/genero', [GeneroController::class, 'store'])->name('guardarGenero');
-Route::get('/usuario/genero/{id}', [UsuarioController::class, 'mostrarPorGenero']);
-Route::post('/cancion/reproducir', [CancionController::class, 'aumentarReproduccion']);
-Route::get('/regalias', [RegaliasController::class, 'index']);
-Route::get('/artista/{id}/regalias', [RegaliasController::class, 'regaliasXArtista']);
+//Rutas de artistas y usuarios
+Route::middleware(['auth'])->group(function () {
+    Route::get('/usuario', [UsuarioController::class, 'showUsuario']);
+    Route::get('/usuario/genero/{id}', [UsuarioController::class, 'mostrarPorGenero']);
+    Route::post('/cancion/reproducir', [CancionController::class, 'aumentarReproduccion']);
+    Route::get('/artista/{id}/regalias', [RegaliasController::class, 'regaliasXArtista']);
+    Route::get('/artista', [ArtistaController::class, 'showArtista']);
+});
+
+// Rutas de administración
+Route::middleware(['auth', PermisosAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'showAdmin']);
+    Route::post('/adminartista', [ArtistaController::class, 'store'])->name('guardarArtista');
+    Route::get('/adminartista', [ArtistaController::class, 'index']);
+    Route::get('/album', [AlbumController::class, 'index']);
+    Route::get('/album/{id}', [AlbumController::class, 'show']);
+    Route::post('/album', [AlbumController::class, 'store'])->name('guardarAlbum');
+    Route::get('/cancion/{id}', [CancionController::class, 'show']);
+    Route::get('/cancion', [CancionController::class, 'index']);
+    Route::post('/cancion', [CancionController::class, 'store'])->name('guardarCancion');
+    Route::get('/genero', [GeneroController::class, 'index']);
+    Route::get('/genero/{id}', [GeneroController::class, 'show']);
+    Route::post('/genero', [GeneroController::class, 'store'])->name('guardarGenero');
+    Route::get('/artista/{id}', [ArtistaController::class, 'show']);
+    Route::get('/regalias', [RegaliasController::class, 'index']);
+});
